d525acdae241939d46b9f34dbf9cb27437ba17b8
Steffy Lo <steffy-lo@hotmail.com>
2020-02-28 09:29:06 -0500

    implemented create account

--- the_rhizone/src/components/Login/index.js
+++ the_rhizone/src/components/Login/index.js
@@ -5,28 +5,28 @@ class Login extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      users: [{username: 'sunshinegirl100', password: 'password123', userId: 0},
-              {username: 'theonehero', password: '1234567', userId: 1},
-              {username: 'user1', password: 'password1', userId: 2}]
+      users: [{username: 'user', password: 'user', userId: 0},
+              {username: 'user2', password: 'user2', userId: 1},
+              {username: 'user3', password: 'user3', userId: 2}]
 
     };
 
     this.authenticate = this.authenticate.bind(this);
+    this.createAccount = this.createAccount.bind(this);
   }
 
   authenticate(e) {
     e.preventDefault();
     console.log(this.state.users);
     const form = document.querySelector('.form');
-    const username = document.querySelector('.username').value;
-    const password = document.querySelector('.password').value;
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
 
     for (let i = 0; i < this.state.users.length; i++) {
       if (this.state.users[i].username == username && this.state.users[i].password == password) {
         console.log("yay");
-        window.location.replace("http://localhost:3000/");
+        document.location.href="/";
         return;
       }
     }
@@ -35,8 +35,18 @@ class Login extends React.Component {
       let pElement = document.createElement('p');
       pElement.className = 'loginError';
       pElement.appendChild(document.createTextNode('Incorrect username or password.'));
+      pElement.style.color = "red";
       form.appendChild(pElement);
     }
+  }
+
+  createAccount(e) {
+    e.preventDefault();
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    this.setState(prevState => ({
+      users: [...prevState.users, { username: username, password: password, userId: prevState.users.length }]
+    }))
   }
 
   render () {
@@ -46,13 +56,12 @@ class Login extends React.Component {
           <h1><a href="/">The RhiZone</a></h1>
         </header>
         <div class="form">
-          <img /* Can include logo here *//>
           <form class="login-form">
-            <input className="username" type="username" placeholder="username"/>
-            <input className="password" type="password" placeholder="password"/>
+            <input id="username" type="username" placeholder="username"/>
+            <input id="password" type="password" placeholder="password"/>
             <button class="login" onClick={this.authenticate}>login</button>
             <p class="message">Not registered? <a>Create an account</a></p><br/>
-            <button class="create">create</button>
+            <button class="create" onClick={this.createAccount}>create</button>
           </form>
         </div>
       </div>
