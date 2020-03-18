450c89c6f0b694bbd4c2692bd4ff4925c09df2fc
Xiaoxue Xu <xiaoxue.xu6397@gmail.com>
2020-03-18 11:07:49 -0400

    sample data loading for user data

---
+++ the_rhizone/models/userDataModel.js
@@ -0,0 +1,22 @@
+/* Student mongoose model */
+const mongoose = require('mongoose')
+
+const userDataModel = mongoose.model('UserData', {
+	userName: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlegth: 1
+	},
+	isAdmin: {
+	    type: Boolean,
+	    default: false
+	}
+})
+
+module.exports = { userDataModel }
